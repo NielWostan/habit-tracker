6987ba6a5c33c043dc247a9cd0ae7d81ed8a5752
Niel Wostan <wostanniel@gmail.com>
2023-12-30 09:58:30 -0600

    improved code

--- Components/CalendarWindow.js
+++ Components/CalendarWindow.js
@@ -1,3 +1,4 @@
+import { getTitle } from "@/library/getTitle";
 import React from "react";
 
 export default function CalendarWindow({
@@ -10,32 +11,59 @@ export default function CalendarWindow({
     toggleChange(id, date);
   }
 
-  let id1 = -1;
-  let id2 = -1;
-  let id3 = -1;
-  let id4 = -1;
+  const dataElements = data.map((dataEl) => (
+    <div
+      className="habitPacket"
+      key={dataEl.id}
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        width: "50%",
+      }}
+    >
+      <p
+        className="habitTitle"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "40px",
+          width: "120px",
+          borderRadius: "8px",
+          backgroundColor: "white",
+          border: "none",
+          fontSize: "18px",
+          padding: "10px",
+        }}
+      >
+        {getTitle(dataEl.habitId)}
+      </p>
+      <div
+        className="habitSlot"
+        onClick={() => registerClick(dataEl.id, `${activeDate}`)}
+        style={{
+          backgroundColor: data
+            .filter((x) => x.habitId == dataEl.habitId)[0]
+            .completedList.includes(activeDate)
+            ? "#324359"
+            : "#d9b6a3",
+          display: "flex",
+          borderRadius: "8px",
+          height: "32px",
+          width: "32px",
+          alignSelf: "center",
+        }}
+      ></div>
+    </div>
+  ));
 
-  for (let i = 0; i < data.length; i++) {
-    switch (data[i].habitId) {
-      case 1:
-        id1 = data[i].id;
-        break;
-      case 2:
-        id2 = data[i].id;
-        break;
-      case 3:
-        id3 = data[i].id;
-        break;
-      case 4:
-        id4 = data[i].id;
-        break;
-    }
-  }
   return (
     <div className="addHabitPopUp">
       <h3 className="addAHabitHeading">Progress for {activeDate}</h3>
       <div className="buttonDiv">
-        <div className="habitPacket">
+        {/*<div className="habitPacket">
           <p>Meditation</p>
           <div
             className="habitSlot meditation"
@@ -62,7 +90,8 @@ export default function CalendarWindow({
             className="habitSlot reading"
             onClick={() => registerClick(id4, `${activeDate}`)}
           ></div>
-        </div>
+        </div*/}
+        {dataElements}
       </div>
       <style jsx>
         {`
@@ -89,72 +118,11 @@ export default function CalendarWindow({
           .buttonDiv {
             display: flex;
             flex-direction: column;
-            justify-content: space-between;
+            justify-content: space-around;
             height: 250px;
             margin-bottom: 40px;
             width: 80%;
             align-items: center;
-          }
-
-          .habitPacket {
-            display: flex;
-            flex-direction: row;
-            justify-content: space-evenly;
-            width: 50%;
-          }
-
-          p {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 40px;
-            width: 100px;
-            border-radius: 8px;
-            background-color: white;
-            border: none;
-            font-size: 18px;
-          }
-
-          .habitSlot {
-            display: flex;
-            background-color: wheat;
-            border-radius: 8px;
-            height: 32px;
-            width: 32px;
-            align-self: center;
-          }
-
-          .meditation {
-            background-color: ${data
-              .filter((x) => x.habitId == 1)[0]
-              .completedList.includes(activeDate)
-              ? "#324359"
-              : "#d9b6a3"};
-          }
-          .workout {
-            background-color: ${data
-              .filter((x) => x.habitId == 2)[0]
-              .completedList.includes(activeDate)
-              ? "#324359"
-              : "#d9b6a3"};
-          }
-          .journal {
-            background-color: ${data
-              .filter((x) => x.habitId == 3)[0]
-              .completedList.includes(activeDate)
-              ? "#324359"
-              : "#d9b6a3"};
-          }
-          .reading {
-            background-color: ${data
-              .filter((x) => x.habitId == 4)[0]
-              .completedList.includes(activeDate)
-              ? "#324359"
-              : "#d9b6a3"};
-          }
-
-          .habitSlot:hover {
-            cursor: pointer;
           }
         `}
       </style>
